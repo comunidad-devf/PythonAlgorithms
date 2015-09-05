7f0c09ad870d2e7d475011de7f02c2a0d645c5e5
Pablo Trinidad <pabloatp0204@gmail.com>
2015-09-04 20:55:03 -0500

    05 Fixing repeated

--- 05 Creting Number Pyramids/number_pyramids.py
+++ 05 Creting Number Pyramids/number_pyramids.py
@@ -1,6 +1,6 @@
 """Number Pyramids."""
 
 n = int(raw_input("Give me a number: "))
-numbers = range(1, n+1) + range(1, n+1)[::-1]
+numbers = range(1, n+1) + range(1, n)[::-1]
 for number in numbers:
     print str(number) * number
