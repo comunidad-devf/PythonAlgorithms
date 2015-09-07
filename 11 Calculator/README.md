6912f21b9f633c3d398e786c168d7a8b4587b984
Pablo Trinidad <pabloatp0204@gmail.com>
2015-09-06 19:00:44 -0500

    11

---
+++ 11 Calculator/README.md
@@ -0,0 +1,3 @@
+# Python Calculator
+
+Escribe la clásica calculadora con opciones usando funciones. Hazla tan compleja como quieras.
---
+++ 11 Calculator/calculator.py
@@ -0,0 +1,55 @@
+"""Python Calculator."""
+
+
+def add(a, b):
+    return a + b
+
+
+def substract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def modulus(a, b):
+    return a % b
+
+
+def compare(a, b):
+    if a > b:
+        return str(a) + " is grather than " + str(b)
+    else:
+        return str(b) + " is grather than " + str(a)
+
+print "\n\t\t_____Python Calculator_____\n"
+print "Which operation below do you want to perform?"
+print "\t1. Add"
+print "\t2. Substract"
+print "\t3. Multiply"
+print "\t4. Divide"
+print "\t5. Modulus"
+print "\t6. Compare"
+o = raw_input()
+
+print "\nOk, enter two numbers:"
+a = float(raw_input("a: "))
+b = float(raw_input("b: "))
+
+if o == '1':
+    print add(a, b)
+elif o == '2':
+    print substract(a, b)
+elif o == '3':
+    print multiply(a, b)
+elif o == '4':
+    print divide(a, b)
+elif o == '5':
+    print modulus(a, b)
+elif o == '6':
+    print compare(a, b)
